03622e33eb6b333be4a16216cf9fbdb0fd22963f
Bellbe <37058707+Bellbe@users.noreply.github.com>
2018-05-17 12:00:58 +1000

    Update flashcards.js

--- flashcards.js
+++ flashcards.js
@@ -2,7 +2,7 @@ var cards = []
 
 function addCard(words) {
   cards.push(words);
-  document.getElementById("cardFront").innerHTML = cards.length;
+  document.getElementById("cardFront").innerHTML = cards[0];
 }
 
 function showCard() {
