691100f44b8c850eab70ec1df4e18d352ecbf7d2
Bellbe <37058707+Bellbe@users.noreply.github.com>
2018-05-17 12:08:09 +1000

    Update flashcards.js

--- flashcards.js
+++ flashcards.js
@@ -1,10 +1,3 @@
-var cards = []
-
 function addCard(words) {
-  <!--cards.push(words);-->
-  document.getElementById("cardFront").innerHTML = words;
-}
-
-function showCard() {
-  document.getElementById("cardFront").innerHTML = cards.length;
+  document.getElementById("cardFront").innerHTML = "words";
 }
